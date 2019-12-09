842d82ae34eb1c7bf67466125e28c0c74da5c703
jmporter1998 <jmporter@ksu.edu>
2019-12-09 16:17:43 -0600

    commit

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,35 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+
+            Vector3 one = new Vector3(3, 4, 5);
+            Vector3 two = new Vector3(4, 8, 0);
+
+            Console.WriteLine($"Static method Add: {Vector3.Add(one, two)}");
+
+            Console.WriteLine($"Static method Add: {one.Add(two)}");
+
+            Console.WriteLine($"Operator: {one + two}");
+
+            Console.WriteLine($"Subtraction: {one - two}");
+
+            Console.WriteLine($"Scalar multiplication: {two * 2}");
+
+            Console.WriteLine($"Should not be equal: {one == two}");
+
+            Console.WriteLine($"Should not be equal: {one == two}");
+
+
+            if (one)
+            {
+                Console.WriteLine($"{one} is truthy");
+
+            }
+            else
+            {
+                Console.WriteLine($"{one} is falsy");
+            }
+            
         }
     }
 }
